87ed92785979f0f6238cd4b59e288771d80c6ee1
hanlizhi <1430261583@qq.com>
2018-03-24 18:39:42 +0800

    init

---
+++ src/main/java/com/thinking/storm/sport/AmqpSpout.java
@@ -0,0 +1,59 @@
+package com.thinking.storm.sport;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.IRichSpout;
+import backtype.storm.topology.OutputFieldsDeclarer;
+
+import java.util.Map;
+
+/**
+ * @author hanlizhi
+ */
+public class AmqpSpout implements IRichSpout {
+
+    @Override
+    public void open(Map map, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void activate() {
+
+    }
+
+    @Override
+    public void deactivate() {
+
+    }
+
+    @Override
+    public void nextTuple() {
+
+    }
+
+    @Override
+    public void ack(Object o) {
+
+    }
+
+    @Override
+    public void fail(Object o) {
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+
+    }
+
+    @Override
+    public Map<String, Object> getComponentConfiguration() {
+        return null;
+    }
+}
